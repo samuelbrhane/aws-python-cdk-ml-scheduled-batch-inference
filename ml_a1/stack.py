03cfa8201621da566b211ef1773238e8a02c183b
samuelbrhanealemayohu <contact@samuelbrhane.com>
2026-02-01 11:19:37 +0300

    Implement S3 input and output buckets in the stack

--- ml_a1/stack.py
+++ ml_a1/stack.py
@@ -1,18 +1,44 @@
 from __future__ import annotations
 
 from constructs import Construct
-from aws_cdk import Stack
-
+from aws_cdk import (
+    Stack,
+    RemovalPolicy,
+    aws_s3 as s3,
+)
 
 class MlA1ScheduledBatchInferenceStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # We will add resources step-by-step:
-        # 1) S3 input/output
-        # 2) SNS for failures
-        # 3) Step Functions workflow
-        # 4) EventBridge Scheduler trigger
-        # 5) CloudWatch logs + alarms
-        # 6) SageMaker model + batch transform integration
+        ### S3 input/output
+        
+        # input
+        self.input_bucket = s3.Bucket(
+            self,
+            "InputBucket",
+            versioned=True,
+            encryption=s3.BucketEncryption.S3_MANAGED,
+            block_public_access=s3.BlockPublicAccess.BLOCK_ALL,
+            removal_policy=RemovalPolicy.DESTROY,
+            auto_delete_objects=True,  
+        )
+
+        # output
+        self.output_bucket = s3.Bucket(
+            self,
+            "OutputBucket",
+            versioned=True,
+            encryption=s3.BucketEncryption.S3_MANAGED,
+            block_public_access=s3.BlockPublicAccess.BLOCK_ALL,
+            removal_policy=RemovalPolicy.DESTROY,
+            auto_delete_objects=True,
+        )
+        
+        
+        # SNS for failures
+        # Step Functions workflow
+        # EventBridge Scheduler trigger
+        # CloudWatch logs + alarms
+        # SageMaker model + batch transform integration
         pass
