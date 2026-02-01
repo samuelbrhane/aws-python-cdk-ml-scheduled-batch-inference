d4599bc17252d5054430fa2c29220043fd483ea1
samuelbrhanealemayohu <contact@samuelbrhane.com>
2026-02-01 11:24:55 +0300

    Refactor S3 bucket comments and add SNS topic for failure alerts

--- ml_a1/stack.py
+++ ml_a1/stack.py
@@ -5,15 +5,14 @@ from aws_cdk import (
     Stack,
     RemovalPolicy,
     aws_s3 as s3,
+    aws_sns as sns,
 )
 
 class MlA1ScheduledBatchInferenceStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        ### S3 input/output
-        
-        # input
+        # S3 (input)
         self.input_bucket = s3.Bucket(
             self,
             "InputBucket",
@@ -24,7 +23,7 @@ class MlA1ScheduledBatchInferenceStack(Stack):
             auto_delete_objects=True,  
         )
 
-        # output
+        # S3 (output)
         self.output_bucket = s3.Bucket(
             self,
             "OutputBucket",
@@ -37,6 +36,12 @@ class MlA1ScheduledBatchInferenceStack(Stack):
         
         
         # SNS for failures
+        self.failure_topic = sns.Topic(
+            self,
+            "FailureAlertsTopic",
+            display_name="ML-A1 Batch Inference Failure Alerts",
+        )
+
         # Step Functions workflow
         # EventBridge Scheduler trigger
         # CloudWatch logs + alarms
